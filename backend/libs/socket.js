fbeb5ccc3ba190cba3cda721b767b5166ba9a1a1
Harsh <userharsh911@gmail.com>
2025-08-04 16:38:04 +0530

    last commit

--- backend/libs/socket.js
+++ backend/libs/socket.js
@@ -6,7 +6,7 @@ const server = createServer(app)
 
 const io = new Server(server,{
     cors:{
-        origin: [process.env.FRONTEND_URI],
+        origin: ["http://localhost:5173"],
         credentials: true,
         methods: ["GET", "POST","PUT", "DELETE"]
     }
