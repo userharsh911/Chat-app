e4e3ea4469e27b52cc221de7559264cb38f6c4aa
Harsh <userharsh911@gmail.com>
2025-08-15 13:06:55 +0530

    updated minor bugs

--- frontend/src/components/RightSidebar.jsx
+++ frontend/src/components/RightSidebar.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import { MessageSquareMore } from 'lucide-react';
 import useBearStore from '../store/store';
 import { CircleArrowLeft } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
 const RightSidebar = () => {
+  const navigate = useNavigate()
   const {setShowUserSideBar, showUserSideBar,userAuth} = useBearStore(state=>state)
   return (
-    <div className={`flex ${!showUserSideBar ? 'flex':'hidden @md:flex'} pb-4 w-full flex-col justify-center items-center bg-primary-content text-primary overflow-hidden`}>
+    <div className={`flex ${(!showUserSideBar ||  !userAuth) ? 'flex':'hidden @md:flex'} pb-4 w-full flex-col justify-center items-center bg-primary-content text-primary overflow-hidden`}>
         
         <div className='animate-bounce'>
             <MessageSquareMore size={50} />
         </div>
-        <div> 
+        <div className='flex flex-col'> 
           {
             !userAuth && (
               <p className='text-center'>Welcome to Chat-ON</p>
@@ -18,8 +20,12 @@ const RightSidebar = () => {
           }
             <p className='text-center text-lg font-semibold'>{!userAuth ? 'Login' : 'Select a person'} to start chatting</p>
             {
-              userAuth && (
+              userAuth ? (
                 <p className='text-center text-sm'>Click on a user profile from the sidebar to begin</p>
+              ) : (
+                <button className='btn mx-auto mt-3 bg-primary text-primary-content' onClick={()=>navigate("/login")}>
+                  Login
+                </button>
               )
             }
 
