3b5339855377c8240898723d6adf363293bc9fbd
Harsh <userharsh911@gmail.com>
2026-02-24 14:56:53 +0530

    cors frontend orirgin set

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ dotenv.config()
 app.use(express.json({limit: '50mb'}))
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials:true
 }))
 
